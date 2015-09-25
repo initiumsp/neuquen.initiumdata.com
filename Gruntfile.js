8b1ee6c645ef1bf0845ee75582bd330e4b131afa
HU Pili <hpl1989@gmail.com>
2015-09-25 10:47:01 +0800

    change to calculated positions

--- Gruntfile.js
+++ Gruntfile.js
@@ -266,7 +266,8 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              js: ['concat', 'uglifyjs'],
+              //js: ['concat', 'uglifyjs'],
+              js: ['uglifyjs'],
               css: ['cssmin']
             },
             post: {}
@@ -496,12 +497,12 @@ module.exports = function (grunt) {
     'useminPrepare',
     'concurrent:dist',
     'autoprefixer',
-    'concat',
-    'ngAnnotate',
+    //'concat',
+    //'ngAnnotate',
     'copy:dist',
     'cdnify',
     'cssmin',
-    'uglify',
+    //'uglify',
     'filerev',
     'usemin',
     'htmlmin'
